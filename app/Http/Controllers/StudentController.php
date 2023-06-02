7b39e93c9ad3945238578d0bec386bd142d77600
Lester <gatpolintanjohn@gmail.com>
2023-06-02 16:23:53 +0800

    new code added and database migration added

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Students;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
     //
+    public function index()
+    {
+        $data = Students::where('id', 1)->get();
+
+        return view('students.index', ['students' => $data]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\StudentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,11 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/index', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/index', function () {
+//     return view('index');
+// });
+Route::get('/', [StudentController::class, 'index']);
 
-Route::get('/', [UserController::class, 'index']);
+// Route::get('/', [UserController::class, 'index'])->name('login');
+// Route::get('/student', [StudentController::class, 'index'])->name('index');
